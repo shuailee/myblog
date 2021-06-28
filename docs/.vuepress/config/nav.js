3fa6e876a8208cd4e4ce0a2b361629c8f49de190
lishuai <lishuai15@glcp.com.cn>
2021-06-28 18:53:13 +0800

    note

---
+++ docs/.vuepress/config/nav.js
@@ -0,0 +1,38 @@
+// nav  
+module.exports = [
+  { text: '首页', link: '/' },
+  {
+    text: '系统架构',
+    link: '/system/', //目录页链接，此处link是vdoing主题新增的配置项，有二级导航时，可以点击一级导航跳到目录页
+    items: [
+      // 说明：以下所有link的值只是在相应md文件定义的永久链接（不是什么特殊生成的编码）。另外，注意结尾是有斜杠的
+      {
+        text: '系统架构',
+        items: [
+          { text: '分布式理论', link: '/pages/73fbb6/' }
+        ],
+      }
+    ],
+  },  
+  
+  
+  { text: '关于', link: '/about/' },
+  {
+    text: '收藏',
+    link: '/pages/beb6c0bd8a66cea6/',
+    // items: [
+    //   { text: '网站', link: '/pages/beb6c0bd8a66cea6/' },
+    //   { text: '资源', link: '/pages/eee83a9211a70f9d/' },
+    //   { text: 'Vue资源', link: '/pages/12df8ace52d493f6/' },
+    // ],
+  },
+  {
+    text: '索引',
+    link: '/archives/',
+    items: [
+      { text: '分类', link: '/categories/' },
+      { text: '标签', link: '/tags/' },
+      { text: '归档', link: '/archives/' },
+    ],
+  },
+]
