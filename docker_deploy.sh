3f79b16be94a2d6be5c958a7d3b7c9e67023126f
lishuai <lishuai15@glcp.com.cn>
2021-06-29 19:16:30 +0800

    i

--- docker_deploy.sh
+++ docker_deploy.sh
@@ -6,7 +6,9 @@ proj_home=$PWD                              # the project root dir
 
 git pull  
 
-echo "use docker node"
+echo "use docker node.js ..."
+echo "npm install..."
+echo "npm run build..."
 docker run --rm \
    -v $proj_home:/usr/src/mynode \
    -w /usr/src/mynode $img_node sh -c 'npm install --registry=https://registry.npm.taobao.org && npm run build '
@@ -14,10 +16,12 @@ docker run --rm \
 # && cp -r docs/.vuepress/dist /usr/share/nginx/html 
 
 
-echo "mv dist start"
+echo "start build image ..."
 docker build -t blog:latest .
-
-docker run -d -p 80:80 blog:latest
+echo "remove old container ..."
+docker rm -f myblog
+echo "run myblog ..."
+docker run -d -p 80:80 blog:latest myblog
 
 
 
